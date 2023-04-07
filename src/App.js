9986ef2981f424d3892077d9d3b3909c92502e9e
Sukanta01 <72244847+Sukanta01@users.noreply.github.com>
2023-04-07 09:52:17 +0530

    restaurant summary added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { Fragment } from 'react';
 
 import Header from './components/Layout/Header';
+import MealsSummary from './components/Meals/MealsSummary';
+//  from './components/Meals/Meals';
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <MealsSummary/>
     </Fragment>
   );
 }
