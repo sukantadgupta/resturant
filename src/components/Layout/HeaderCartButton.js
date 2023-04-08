b99774bb3da262773ee803a2fac52a0110fcad23
Sukanta01 <72244847+Sukanta01@users.noreply.github.com>
2023-04-08 21:17:44 +0530

    cart function

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -3,13 +3,13 @@ import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
   return (
-    <button className={classes.button}>
-      <span className={classes.icon}>
-        <CartIcon />
-      </span>
-      <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
-    </button>
+    <button className={classes.button} onClick={props.onClick}>
+    <span className={classes.icon}>
+      <CartIcon />
+    </span>
+    <span>Your Cart</span>
+    <span className={classes.badge}>3</span>
+  </button>
   );
 };
 
